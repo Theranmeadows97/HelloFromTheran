500d7c41c9c5cc626fa83e6d7bf1b8b7d0837f2f
cbbrac <cbbrac@byui.edu>
2021-05-20 17:01:44 -0300

    Added a message from Brian

--- src/week05/Main.java
+++ src/week05/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Hello, my name is Theran.");
 	System.out.println("Hello, Theran. My name is Melisa");
+    System.out.println("Hello, Therannn. I love you man! <3");
     }
 }
